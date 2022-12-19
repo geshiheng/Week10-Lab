8ef009bcf65c56c3bdeff0d3b1e8da598e76f57b
geshiheng <ge952893553@gmail.com>
2022-12-18 19:30:30 -0800

    modified:   server.py

--- server.py
+++ server.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Import your game engine and any other necessary modules here
+import game_engine
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/make_move/<int:row>/<int:col>')
+def make_move(row, col):
+    # Use the game engine to make a move at the specified row and column
+    result = game_engine.make_move(row, col)
+    return result
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
